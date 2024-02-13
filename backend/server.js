f9a96201a76bf10461f51f596713e438acd4a19f
Cole <153543951+McFlanky@users.noreply.github.com>
2024-02-13 13:28:33 -0500

    4th part completed - socket.io implementation

--- backend/server.js
+++ backend/server.js
@@ -2,13 +2,13 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js"
-import messageRoutes from "./routes/message.routes.js"
-import userRoutes from "./routes/user.routes.js"
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
@@ -16,16 +16,16 @@ dotenv.config();
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes);  
-app.use("/api/messages", messageRoutes); 
-app.use("/api/users", userRoutes); 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 // app.get("/", (req, res) => {
-    // root route http://localhost:5000/
+// root route http://localhost:5000/
 //     res.send("HELLO WORLD!!")
 // });
 
-app.listen(PORT, () => {
-    connectToMongoDB();
-    console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, () => {
+  connectToMongoDB();
+  console.log(`Server is running on port ${PORT}`);
 });
